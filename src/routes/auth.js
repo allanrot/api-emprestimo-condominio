d3aded1c5fff4b8d879422904e9c13d6460dbdc5
Allan Rothenburg <allantrickrothenburg@gmail.com>
2026-06-08 10:53:22 -0300

    fix: import name

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const User = require('../models/user-model');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
--- src/routes/items.js
+++ src/routes/items.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Item = require('../models/item');
+const Item = require('../models/item-model');
 const auth = require('../middlewares/auth');
 
 router.get('/', async (req, res) => {
